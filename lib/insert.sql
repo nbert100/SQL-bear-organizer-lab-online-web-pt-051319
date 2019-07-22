f96dd609432c4214e29d711eeb3fb2bb93ea9842
Nia Bert <nbert14@gmail.com>
2019-07-22 03:09:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'female');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'female', 'brown', 'mean', 0);
